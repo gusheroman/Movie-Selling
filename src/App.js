5b7ed9316a4b8a160647089200d8645ed72274ec
gusheroman1 <tanadon.gus@mail.kmutt.ac.th>
2022-08-20 17:42:12 +0700

    connect API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
 function App() {
+ const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=f0b959cbcbffe50ac134dda60eaa7e74";
+ const [movie, setMovies]=useState([]);
+
+ useEffect(()=>{
+  fetch(API_URL)
+  .then((res)=>res.json())
+  .then(data=>{
+    console.log(data)
+    setMovies(data.results);
+  })
+ },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movie.map((items)=>
+      <MovieCard key={items.id} {...items} />
+      )}
     </div>
   );
 }
